3e906ef647a18b0a80d9ebb19c932bae4d7e22b1
Nikolas-ba-ara <146142269+Nikolas-ba-ara@users.noreply.github.com>
2025-10-23 13:53:37 -0300

    push fastapi sola 7

--- FastApi/fastapi_test.py
+++ FastApi/fastapi_test.py
@@ -28,7 +28,7 @@ def read_root():
 
 @app.get("/test_docker_deploy")
 def docker_deploy():
-    return {"status": "prueba fastapi sola 6"}
+    return {"status": "prueba fastapi sola 7"}
 
 
 @app.get("/debug/config")
